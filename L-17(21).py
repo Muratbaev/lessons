f18c8bb4a63b148e60485523c4c2ef57834b62f7
Muratbaev <71893574+Muratbaev@users.noreply.github.com>
2025-12-11 17:10:18 +0500

    продолжение 11.12.25

---
+++ L-17(21).py
@@ -0,0 +1,60 @@
+# def num(a):
+#     print('sup', a)
+
+# for i in range(4):
+#     a = input('name - ')
+#     num(a)
+
+
+# user = input('text - ')
+# def ceck(a):
+#     if user.lower()==user:
+#         print(f'{a} написан мелкими')
+#     if user.upper()==user:
+#         print(f'{a} написан большими ')
+# ceck(a=user)
+
+# names = ['aza', 'alish', 'indi', 'malika']
+# def length():
+#     for i in names:
+#         print(f'{i} - {len(i)} harip')
+# length()    
+
+# print('\n')
+
+# file = open(file='file1.txt', mode='r')
+# copy = file.read()
+# def file_read():
+#     print(copy)
+# file_read()
+
+# def max(a, b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# print(max(a = int(input('a- ')), b = int(input('b- '))))
+
+# def max(a, b):
+#     if a>b:
+#         print(a)
+#     else:
+#         print(b)
+# print(max(a = int(input('a- ')), b = int(input('b- '))))
+
+# print(range(10))
+# print(range(5, 10))
+
+def qos(a=0, b=0, c=0, d=0, e=9, f = 100):
+    return a+b+c+d+e+f
+print(qos())
+
+# def qos(a=0, b=0, c=0, d=-9, e=-4):
+#     return a+b+c+d+e+113
+# print(qos(1))
+
+
+
+# def sasa(a=0, b=0):
+#     return a+b
+# print(sasa(10, 100))
