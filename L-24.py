918bbe1f100ec8e021b516e6a0fe49b7989c649b
Muratbaev <71893574+Muratbaev@users.noreply.github.com>
2025-12-18 16:06:27 +0500

    commit

--- L-24.py
+++ L-24.py
@@ -62,4 +62,76 @@ def tovsum():
         price = int(input('цена: '))
         tovary[name]=price
     return sum(tovary.values())
-print(tovsum())
+# print(tovsum())
+
+#HOMETASK
+# 1 сорт списка чисел по убыв
+def tros():
+    user = []
+    times = int(input('how many nums you wanna add? - '))
+    for i in range(times):
+        num = int(input('type the nums - '))
+        user.append(num)
+
+    user_sorted = user.sort(reverse=True)
+    print(f'the reversed list of your nums: {user_sorted}')
+
+#2 max from three nums
+def maxx():
+    nums = []
+    for i in range(3):
+        num = int(input('type the num - '))
+        nums.append(num)
+    nums.sort()
+    return nums[-1]
+# maxx()
+
+#3 panagramma check
+alphabet = []
+abc = 'abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ'
+for let in abc:
+    alphabet.append(let)
+word = []
+def alp():
+    user = input('text below - ')
+    for let in user:
+        word.append(let)
+    if word not in alphabet:
+        print('it\'s not panagramma')
+    else:
+        print('it is panagramma')
+
+
+# 4 Напишите программу на Python для доступа к функции внутри функции.
+def my_0():
+    def my_1():
+        return 'внутренняя фукнция'
+    return my_1
+my_1=my_0
+#print(my_1())
+
+#5 Напишите программу на Python для определения количества локальных переменных, объявленных в функции. 
+def loc():
+    a = 10
+    b = 'slovo'
+    c = ['slovo1', 2]
+    d = {'k1':'v1'}
+    local_vars = locals()
+    count = len(local_vars)
+    names = list(local_vars.keys())
+    return f'локальных переменных {count}, а их названия: {names}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
